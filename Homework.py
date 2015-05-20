6f6de54ca5b1d8ec1db196876def365925605ca8
gerardofranco99 <18gfranco@iaa.edu.ec>
2015-05-20 10:01:12 -0500

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,6 @@
+import sys
+print "your name is: ", sys.argv[1]
+print 'your last name is: ', sys.argv[2]
+print "I was born in: ", sys.argv[3]
+print "I graduate in: ", sys.argv[4]
+print "My gpa is: ", sys.argv[5]
